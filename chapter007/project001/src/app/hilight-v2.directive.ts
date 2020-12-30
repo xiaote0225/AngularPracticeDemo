90b5bd8286942985790c39243f363e4af7f1ecb7
wangxun <wangxun0225@163.com>
2020-12-31 00:12:26 +0800

    feat:绑定多个属性;

--- chapter007/project001/src/app/hilight-v2.directive.ts
+++ chapter007/project001/src/app/hilight-v2.directive.ts
@@ -7,10 +7,12 @@ export class HilightV2Directive {
 
   constructor(private el:ElementRef) { }
 
+  @Input() defaultColor:string | undefined;
+
   @Input('appHilightV2') highlightColor:string | undefined;
 
   @HostListener('mouseenter') onMouseEnter(){
-    this.highlight(this.highlightColor || 'red');
+    this.highlight(this.highlightColor || this.defaultColor || 'red');
   }
 
   @HostListener('mouseleave') onMouseLeave(){
