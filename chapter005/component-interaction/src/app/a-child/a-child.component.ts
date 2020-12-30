653fe4f4282ee3d76060b5dbe9a655c3cb0de744
wangxun <wangxun@cyai.com>
2020-12-30 14:43:02 +0800

    feat:控制视图的封装模式,更改组件的封装模式需要修改组件元数据中的encapsulation属性来设置;

--- chapter005/component-interaction/src/app/a-child/a-child.component.ts
+++ chapter005/component-interaction/src/app/a-child/a-child.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-a-child',
   templateUrl: './a-child.component.html',
-  styleUrls: ['./a-child.component.scss']
+  styleUrls: ['./a-child.component.scss'],
+  // encapsulation:ViewEncapsulation.ShadowDom
+  // encapsulation:ViewEncapsulation.Emulated
+  // encapsulation:ViewEncapsulation.None
 })
 export class AChildComponent implements OnInit {
 
