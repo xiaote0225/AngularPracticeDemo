43e210bc7a07868b2d888d6844f262848519d10a
wangxun <wangxun@cyai.com>
2021-01-05 14:14:55 +0800

    feat:响应式表单的例子;

---
+++ chapter010/reactive-forms/src/app/name-editor/name-editor.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-name-editor',
+  templateUrl: './name-editor.component.html',
+  styleUrls: ['./name-editor.component.css']
+})
+export class NameEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  name = new FormControl(''); //实例化
+
+  updateName(){
+    this.name.setValue('老卫');
+  }
+
+}
