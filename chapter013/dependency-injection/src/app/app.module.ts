2f9c69964a7f172d6c939277d5989f9d9db877b6
wangxun <wangxun@cyai.com>
2021-01-06 16:25:33 +0800

    feat:创建users与user-list组件;

--- chapter013/dependency-injection/src/app/app.module.ts
+++ chapter013/dependency-injection/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
