f40ac1efe3b579c5898ca5e47a0d9b1b1f5c4e21
wangxun <wangxun@cyai.com>
2021-01-06 16:41:40 +0800

    feat:依赖注入demo；

--- chapter013/dependency-injection/src/app/user-list/user-list.component.ts
+++ chapter013/dependency-injection/src/app/user-list/user-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../users/user';
+import { UserService } from './../users/user.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  users:User[];
 
-  constructor() { }
+  constructor(userService:UserService) {
+    this.users = userService.getHeroes();
+  }
 
   ngOnInit(): void {
   }
