3cc55eb5c75f53f1cdc7bb2f9eee8b269433badc
wangxun <wangxun@cyai.com>
2021-01-07 13:55:25 +0800

    feat:一个路由器的例子;

--- chapter015/router/src/app/app-routing.module.ts
+++ chapter015/router/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+//路由器配置
+const routes: Routes = [
+  {path:'dashboard',component:DashboardComponent},
+  {path:'users',component:UserListComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- chapter015/router/src/app/app.module.ts
+++ chapter015/router/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UserListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
