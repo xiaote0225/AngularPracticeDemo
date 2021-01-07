069d0fb1be7b2321d4add0717e338a39be40811f
wangxun <wangxun0225@163.com>
2021-01-07 23:36:45 +0800

    feat:单独测试组件类;

--- chapter016/testing/src/app/app.module.ts
+++ chapter016/testing/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LightswitchComponent } from './lightswitch/lightswitch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LightswitchComponent
   ],
   imports: [
     BrowserModule,
