28f161a3034d7a49910803d07185240f90d0f25d
wangxun <wangxun@cyai.com>
2021-01-18 14:07:07 +0800

    feat:修改根模块AppModule;

--- chapter019/universal/src/app/app.module.ts
+++ chapter019/universal/src/app/app.module.ts
@@ -9,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    // 添加.withServerTransition()以支持Universal渲染
+    // 应用ID可以是页面上唯一的任何标识符
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
     AppRoutingModule
   ],
   providers: [],
